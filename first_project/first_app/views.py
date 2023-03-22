d9b3c2d49c7527281706d52071e54402a13fef0b
Sirius <vivekchaurasia260@gmail.com>
2023-03-22 19:36:58 +0530

    successfully updated the secret key.

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    #return HttpResponse("Hello World!")
+    
+    #create a dictionary
+    my_dict = {'insert_me': "Now I modified the index html path!"}
+    return render(request,'first_app/index.html',context=my_dict)
--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -14,13 +14,14 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = Path.joinpath(BASE_DIR, "templates")
+STATIC_DIR = Path.joinpath(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dbq6ejr0y#tqwo0s7xb&tj$^g^39x720dg#mc7iyub7dafj%#6'
+SECRET_KEY = "your secret key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
