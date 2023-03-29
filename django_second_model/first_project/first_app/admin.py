04e3bb8585cf8c72bc3099f70b439cfae9ded187
Sirius <vivekchaurasia260@gmail.com>
2023-03-29 23:49:33 +0530

    Second model folder committed successfully.

---
+++ django_second_model/first_project/first_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from first_app.models import AccessRecord, Webpage, Topic
+
+# Register your models here.
+
+admin.site.register(AccessRecord)
+admin.site.register(Webpage)
+admin.site.register(Topic)
+
